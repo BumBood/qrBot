49b96ff8100087a76bfb75e1be276f22e7dbb442
BumBood <enerrgoworld@mail.ru>
2025-09-01 16:37:44 +0300

    Изменен тип данных для поля winner_user_id с Integer на BigInteger в модели WeeklyLottery для поддержки больших значений. Обновлен импорт для улучшения читаемости кода.

--- src/models/weekly_lottery_model.py
+++ src/models/weekly_lottery_model.py
@@ -1,4 +1,12 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
+from sqlalchemy import (
+    Column,
+    Integer,
+    BigInteger,
+    String,
+    DateTime,
+    ForeignKey,
+    Boolean,
+)
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from database import Base
@@ -13,7 +21,7 @@ class WeeklyLottery(Base):
     week_start = Column(DateTime, nullable=False)  # Начало недели (понедельник 00:00)
     week_end = Column(DateTime, nullable=False)  # Конец недели (воскресенье 23:59)
     winner_user_id = Column(
-        Integer, ForeignKey("users.id"), nullable=True
+        BigInteger, ForeignKey("users.id"), nullable=True
     )  # ID победителя
     winner_receipt_id = Column(
         Integer, ForeignKey("receipts.id"), nullable=True
